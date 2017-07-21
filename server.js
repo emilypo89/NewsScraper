4e93ae7be8012793948e17296d8c5a2836052682
emilypo89 <emilypo89@gmail.com>
2017-07-21 15:25:27 -0400

    heroku

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
-// ## Instructions
-
-// * Create an app that follows this user story:
-
-//   1. Whenever a user visits your site, the app will scrape stories from a news outlet of your choice. The data should at least include a link to the story and a headline, but feel free to add more content to your database (photos, bylines, and so on).
-//   2. Use Cheerio to grab the site content and Mongoose to save it to your MongoDB database. 
-
-//   3. All users can leave comments on the stories you collect. They should also be allowed to delete whatever comments they want removed. All stored comments should be visible to every user.
-//   4. You'll need to use Mongoose's model system to associate comments with particular articles. 
-
-// ### Tips
-
-// * Go back to Saturday's activities if you need a refresher on how to partner one model with another.
-
-// * Whenever you scrape a site for stories, make sure an article isn't already represented in your database before saving it; we don't want duplicates. 
-
-// * Don't just clear out your database and populate it with scraped articles whenever a user accesses your site. 
-//   * If your app deletes stories every time someone visits, your users won't be able to see any comments except the ones that they post.
 
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var methodOverride = require("method-override");
@@ -209,7 +194,7 @@ app.delete("/saved/delete/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT);
 });
 
